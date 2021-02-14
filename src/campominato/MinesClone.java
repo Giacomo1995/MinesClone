559b394b39f7655a1f3b62320487c10827c6d5a4
Giacomo1995 <giamel95@outlook.it>
2021-02-14 12:36:27 +0100

    Add files via upload

---
+++ src/campominato/MinesClone.java
@@ -0,0 +1,9 @@
+package campominato;
+
+public class MinesClone {
+
+    public static void main(String[] args) {
+        GUI.getInstance(Level.EASY); // By default the difficulty is set to easy
+    }
+
+}
